7051d2b5b3dc94ba2c1189b51535bc6d3502f385
chakeradel <chakeradel30@gmail.com>
2024-09-26 11:28:07 +0100

    conversion pages done

--- frontend/src/app/homePage/page.tsx
+++ frontend/src/app/homePage/page.tsx
@@ -1,15 +1,17 @@
+'use client'
+
 import React from "react";
 import { useSelector } from "react-redux";
-import { RootState } from "../redux/store";
-import Banner from "../components/HomePage/Banner";
-import Foods from "../components/HomePage/Foods";
-import Footer from "../components/HomePage/Footer/Footer";
-import RestaurantList from "../components/RestaurantList";
-import HowItWorks from "../components/HomePage/HowItWorks";
-import Testimonials from "../components/HomePage/Testimonials";
-import SearchResults from "../components/SearchResults";
+import { RootState } from "../../redux/store";
+import Banner from "../../components/HomePage/Banner";
+import Foods from "../../components/HomePage/Foods";
+import Footer from "../../components/HomePage/Footer/Footer";
+import RestaurantList from "../../components/RestaurantList";
+import HowItWorks from "../../components/HomePage/HowItWorks";
+import Testimonials from "../../components/HomePage/Testimonials";
+import SearchResults from "../../components/SearchResults";
 
-const HomePage: React.FC = () => {
+export const HomePage: React.FC = () => {
   const { results } = useSelector((state: RootState) => state.search);
   const hasSearchResults =
     results.menuItems.length > 0 || results.restaurants.length > 0;
