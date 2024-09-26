c760825b82cfff50cd767c8ce73e1242c68d3c31
NciriAbdelmajid <abdelmajidnciri872@gmail.com>
2024-09-26 15:44:47 +0100

    Menu,Order,Delivery

--- backend/routes/User.route.js
+++ backend/routes/User.route.js
@@ -1,17 +1,17 @@
 // userRoutes.js
 
-const express = require('express');
-const userController = require('./userController');
+const express = require("express");
+const userController = require("../controllers/User.controller");
 
 const router = express.Router();
 
-router.get('/', userController.getAllUsers);
-router.get('/:id', userController.getUserById);
-router.post('/', userController.createUser);
-router.put('/:id', userController.updateUser);
-router.get('/restaurants', userController.getAllUsersRestaurant);
-router.post('/findNearby', userController.findNearbyRestaurants);
-router.delete('/:id', userController.deleteUser);
-router.put('/location', userController.updateUserLocation);
+router.get("/", userController.getAllUsers);
+router.get("/:id", userController.getUserById);
+router.post("/", userController.createUser);
+router.put("/:id", userController.updateUser);
+router.get("/restaurants", userController.getAllUsersRestaurant);
+router.post("/findNearby", userController.findNearbyRestaurants);
+router.delete("/:id", userController.deleteUser);
+router.put("/location", userController.updateUserLocation);
 
 module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,14 @@
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
+
+const userRoute = require("./routes/User.route");
+
+const orderRoutes = require("./routes/Order.route");
+const menuItemRoutes = require("./routes/MenuItem.route");
+const deliveryRoutes = require("./routes/Delivery.route");
 // const { connectToDatabase } = require("./prisma"); // Import connectToDatabase function
-const {connectToDatabase } = require("./prisma/prisma")
+const { connectToDatabase } = require("./prisma/prisma");
 // App configuration
 const app = express();
 
@@ -12,18 +18,22 @@ app.use(cors()); // Handle cross-origin requests
 app.use(express.json()); // Parse JSON request bodies
 app.use(morgan("dev")); // Log HTTP requests
 
+//Order/MenuItem/Delivery
+app.use("/api/orders", orderRoutes);
+app.use("/api/menu-items", menuItemRoutes);
+app.use("/api/deliveries", deliveryRoutes);
+
+app.use("/api/user", userRoute);
 //* Import & Register Routes
 // Example Route
-const sampleRoute = require("./routes/sample.route");
-app.use("/api/sample", sampleRoute);
-
+// app.use();
 // Start the server
 const PORT = process.env.SERVER_PORT || 3306; // Provide a default port
 app.listen(PORT, async () => {
   console.log("-".repeat(30));
   console.log(`🟢 Server running on : http://localhost:${PORT}`);
   console.log("-".repeat(30));
-  
+
   // Connect to the database when the server starts
   await connectToDatabase();
 });
