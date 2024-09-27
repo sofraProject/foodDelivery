98e7b87f76fed074c53046a2f091c5775f4d8b44
azyzkabada <azyz.kabada@gmail.com>
2024-09-27 17:27:34 +0100

    pay log update2

--- frontend/src/app/success/page.tsx
+++ frontend/src/app/success/page.tsx
@@ -1,31 +1,34 @@
-'use client'
+"use client";
 
+import Image from "next/image";
+import { useRouter, useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
 import { MdVerified } from "react-icons/md";
-import { useNavigate, useLocation } from "react-router-dom";
-import GridLoader from "react-spinners/GridLoader";
-import orderSuccessful from "../assets/ordersuccess.png";
-import { clearCartAsync } from "../../redux/features/cartSlice";
 import { useDispatch } from "react-redux";
+import GridLoader from "react-spinners/GridLoader";
+import { clearCart } from "../../redux/features/cartSlice";
 import { AppDispatch } from "../../redux/store";
+
 const OrderSuccessfulScreen = () => {
   const dispatch = useDispatch<AppDispatch>();
   const [loading, setLoading] = useState(true);
-  const navigate = useNavigate();
-  const location = useLocation();
-  const orderId = new URLSearchParams(location.search).get("orderId");
+  const router = useRouter();
+  const searchParams = useSearchParams(); // Utilisation de useSearchParams pour accéder aux paramètres d'URL
 
+  // Récupération de l'ID de la commande
+  const orderId = searchParams.get("orderId");
+  console.log("----------", orderId);
   useEffect(() => {
     setLoading(true);
-    dispatch(clearCartAsync());
+    dispatch(clearCart());
     setTimeout(() => {
       setLoading(false);
     }, 3000);
-  }, []);
+  }, [dispatch]);
 
   const handleTrackOrder = () => {
     if (orderId) {
-      navigate(`/delivery-tracking/${orderId}`);
+      router.push(`/delivery-tracking/${orderId}`);
     } else {
       console.error("Order ID not found");
     }
@@ -33,31 +36,33 @@ const OrderSuccessfulScreen = () => {
 
   return (
     <main className="h-screen banner">
-      <div className="max-w-screen-xl py-20 mx-auto px-6">
+      <div className="max-w-screen-xl px-6 py-20 mx-auto">
         {loading ? (
-          <div className="flex flex-col items-center justify-center h-3/4 pt-24">
+          <div className="flex flex-col items-center justify-center pt-24 h-3/4">
             <GridLoader color="#ce193c" loading={loading} size={25} />
           </div>
         ) : (
           <>
-            <div className="flex flex-col items-center justify-center h-3/4 pt-24">
-              <h1 className="text-3xl text-center text-primary font-semibold poppins flex space-x-6 items-center ">
-                <MdVerified className="text-primary green-500 text-3xl" /> Order
+            <div className="flex flex-col items-center justify-center pt-24 h-3/4">
+              <h1 className="flex items-center space-x-6 text-3xl font-semibold text-center text-primary poppins ">
+                <MdVerified className="text-3xl text-primary green-500" /> Order
                 Successful!!!
               </h1>
-              <img
-                className="w-96 object-contain"
-                src={orderSuccessful}
-                alt="orderSuccessful"
+              <Image
+                className="object-contain w-96"
+                src="/ordersuccess.png" // Assurez-vous que cette image est placée dans le dossier "public"
+                alt="Order Successful"
+                width={400}
+                height={300}
               />
-              <div className="flex flex-col space-y-4 mt-8 w-full max-w-xs mx-auto">
+              <div className="flex flex-col w-full max-w-xs mx-auto mt-8 space-y-4">
                 <button
-                  className="bg-primary text-white px-8 py-2 focus:outline-none poppins rounded-full transform transition duration-300 hover:scale-105 flex items-center justify-center"
-                  onClick={() => navigate("/")}
+                  className="flex items-center justify-center px-8 py-2 text-white transition duration-300 transform rounded-full bg-primary focus:outline-none poppins hover:scale-105"
+                  onClick={() => router.push("/")}
                 >
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
-                    className="h-5 w-5 mr-2"
+                    className="w-5 h-5 mr-2"
                     viewBox="0 0 20 20"
                     fill="currentColor"
                   >
@@ -71,12 +76,12 @@ const OrderSuccessfulScreen = () => {
                 </button>
                 {orderId && (
                   <button
-                    className="bg-orange-500 text-white px-8 py-2 focus:outline-none poppins rounded-full transform transition duration-300 hover:scale-105 flex items-center justify-center"
+                    className="flex items-center justify-center px-8 py-2 text-white transition duration-300 transform bg-orange-500 rounded-full focus:outline-none poppins hover:scale-105"
                     onClick={handleTrackOrder}
                   >
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
-                      className="h-5 w-5 mr-2"
+                      className="w-5 h-5 mr-2"
                       viewBox="0 0 20 20"
                       fill="currentColor"
                     >
