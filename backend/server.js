e8dca2c68eee73c8fc4286afde46d7b14b62455e
chakeradel <chakeradel30@gmail.com>
2024-10-01 14:12:41 +0100

    admin done

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ const helmet = require("helmet");
 const cookieParser = require("cookie-parser");
 const http = require("http");
 require("dotenv").config();
+const bodyParser = require('body-parser');
 
 // Import routes
 const orderRoutes = require("./routes/Order.route");
@@ -43,6 +44,8 @@ app.use('/uploads', express.static('uploads'));
 helmet({
   crossOriginResourcePolicy: false,
 })
+app.use(bodyParser.json({ limit: '100mb' })); // Vous pouvez ajuster la limite selon vos besoins
+app.use(bodyParser.urlencoded({ limit: '100mb', extended: true }))
 
 // Define API routes
 app.use("/api/orders", orderRoutes);
