b83d2ac897172ef27b220b1ce460acccba45aa9d
AHMD-GTAT <hameda.ahmed2929@gmail.com>
2024-10-01 13:44:44 +0100

    updated user profile attempt 2

--- frontend/src/components/Account/UpdateProfile.tsx
+++ frontend/src/components/Account/UpdateProfile.tsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react';
-import Image from 'next/image';
 import axios from 'axios';
 import Modal from './Modal';
 import { useAuth } from '../../hooks/useAuth';
+import Image from 'next/image';
 
 const serverDomain = process.env.NEXT_PUBLIC_SERVER_DOMAINE;
 
@@ -12,7 +12,6 @@ const UpdateProfile = () => {
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [newPassword, setNewPassword] = useState('');
-    const [profilePicture, setProfilePicture] = useState('');
     const [isEditingName, setIsEditingName] = useState(false);
     const [isEditingEmail, setIsEditingEmail] = useState(false);
     const [isEditingPassword, setIsEditingPassword] = useState(false);
@@ -25,7 +24,6 @@ const UpdateProfile = () => {
                     setUserData(response.data);
                     setName(response.data.name);
                     setEmail(response.data.email);
-                    setProfilePicture(response.data.imageUrl);
                 } catch (error) {
                     console.error("Error retrieving user data", error);
                 }
@@ -61,78 +59,52 @@ const UpdateProfile = () => {
         }
     };
 
+    if (!userData) return <div>Loading...</div>;
+
     return (
-        <div className="flex items-start justify-start min-h-screen bg-gradient-to-r from-primary to-neutral p-4 mt-4">
-            <div className="w-full max-w-md">
-                <h2 className="text-xl font-bold mb-4 text-left">Update Profile</h2>
-
+        <div className="flex items-start justify-start min-h-screen bg-gradient-to-r from-primary to-neutral p-4">
+            <div className="w-full max-w-md mt-24">
+                <h2 className="text-2xl font-bold mb-4 text-left">Update Profile</h2>
+    
                 <div className="flex justify-between mb-4">
-                    <span className="block text-sm font-medium">Name: {name}</span>
-                    <span className="text-blue-600 cursor-pointer" onClick={() => setIsEditingName(true)}>Edit</span>
+                    <span className="block text-lg font-semibold">Name: {name}</span>
+                    <span className="text-blue-600 cursor-pointer ml-auto" onClick={() => setIsEditingName(true)}>Edit</span>
                 </div>
-
+    
                 <div className="flex justify-between mb-4">
-                    <span className="block text-sm font-medium">Email: {email}</span>
-                    <span className="text-blue-600 cursor-pointer" onClick={() => setIsEditingEmail(true)}>Edit</span>
+                    <span className="block text-lg font-semibold">Email: {email}</span>
+                    <span className="text-blue-600 cursor-pointer ml-auto" onClick={() => setIsEditingEmail(true)}>Edit</span>
                 </div>
-
+    
                 <div className="flex justify-between mb-4">
-                    <span className="block text-sm font-medium">Change Password:</span>
-                    <span className="text-blue-600 cursor-pointer" onClick={() => setIsEditingPassword(true)}>Edit</span>
+                    <span className="block text-lg font-semibold">Change Password:</span>
+                    <span className="text-blue-600 cursor-pointer ml-auto" onClick={() => setIsEditingPassword(true)}>Edit</span>
                 </div>
-
-                {/* Profile Picture */}
-                <h2 className="text-xl font-bold mt-6 mb-4 text-left">Profile Picture</h2>
-                <div className="flex items-center justify-center mb-4">
-                    <Image
-                        src={`${serverDomain}/${profilePicture ? profilePicture.replace(/\\/g, '/') : 'uploads/default-avatar.jpg'}`}
-                        alt="Profile"
-                        width={160}
-                        height={160}
-                        className="rounded-full border-4 border-accent shadow-lg transform transition-transform duration-300 hover:scale-110"
-                    />
-                </div>
-
+    
                 {isEditingName && (
-                    <Modal title="Edit your name" onClose={() => setIsEditingName(false)}>
+                    <Modal title="Edit your name" onClose={() => setIsEditingName(false)} onSave={handleUpdateName}>
                         <input
                             type="text"
                             value={name}
                             onChange={(e) => setName(e.target.value)}
                             className="block w-full p-2 border border-gray-300 rounded-md"
                         />
-                        <div className="flex justify-between mt-4">
-                            <button onClick={() => setIsEditingName(false)} className="px-4 py-2 bg-gray-400 text-white rounded-md hover:bg-gray-500">
-                                Cancel
-                            </button>
-                            <button onClick={handleUpdateName} className="px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700">
-                                Save
-                            </button>
-                        </div>
                     </Modal>
                 )}
-
+    
                 {isEditingEmail && (
-                    <Modal title="Edit your email" onClose={() => setIsEditingEmail(false)}>
+                    <Modal title="Edit your email" onClose={() => setIsEditingEmail(false)} onSave={handleUpdateEmail}>
                         <input
                             type="email"
                             value={email}
                             onChange={(e) => setEmail(e.target.value)}
                             className="block w-full p-2 border border-gray-300 rounded-md"
                         />
-                        <div className="flex justify-between mt-4">
-                            <button onClick={() => setIsEditingEmail(false)} className="px-4 py-2 bg-gray-400 text-white rounded-md hover:bg-gray-500">
-                                Cancel
-                            </button>
-                            <button onClick={handleUpdateEmail} className="px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700">
-                                Save
-                            </button>
-                        </div>
                     </Modal>
                 )}
-
+    
                 {isEditingPassword && (
-                    <Modal title="Change your password" onClose={() => setIsEditingPassword(false)}>
+                    <Modal title="Change your password" onClose={() => setIsEditingPassword(false)} onSave={handleUpdatePassword}>
                         <input
                             type="password"
                             placeholder="Enter new password"
@@ -140,19 +112,29 @@ const UpdateProfile = () => {
                             onChange={(e) => setNewPassword(e.target.value)}
                             className="block w-full p-2 border border-gray-300 rounded-md"
                         />
-                        <div className="flex justify-between mt-4">
-                            <button onClick={() => setIsEditingPassword(false)} className="px-4 py-2 bg-gray-400 text-white rounded-md hover:bg-gray-500">
-                                Cancel
-                            </button>
-                            <button onClick={handleUpdatePassword} className="px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700">
-                                Save
-                            </button>
-                        </div>
                     </Modal>
                 )}
+    
+                <h2 className="text-xl font-bold mt-6 mb-4 text-left">Update Profile Picture</h2>
+                <form className="space-y-4">
+                    <div>
+                        <Image
+                            src={`${serverDomain}/${userData.imageUrl}`}
+                            alt="Profile"
+                            width={160}
+                            height={160}
+                            className="rounded-full border-4 border-accent shadow-lg"
+                        />
+                    </div>
+                    <button type="submit" className="mt-4 px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700">
+                        Update Picture
+                    </button>
+                </form>
             </div>
         </div>
     );
+    
+    
 };
 
 export default UpdateProfile;
