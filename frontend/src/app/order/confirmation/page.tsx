acd30ec81c70763a10601b53c0840622919bfcd4
azyzkabada <azyz.kabada@gmail.com>
2024-10-01 13:51:42 +0100

    CHECK

--- frontend/src/app/order/confirmation/page.tsx
+++ frontend/src/app/order/confirmation/page.tsx
@@ -1,18 +1,23 @@
 "use client";
+import axios from "axios";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import swal from "sweetalert";
-import successImage from "../../../assets/succes.svg"; // Image de succès pour améliorer le design
-type ButtonList = string | boolean | false; // Ajouter 'false' dans le type
+import successImage from "../../../assets/succes.svg"; // Success image for improved design
+import { removeItemsByRestaurantId } from "../../../redux/features/cartSlice"; // Import the action
+const serverDomain = process.env.NEXT_PUBLIC_SERVER_DOMAINE;
 
 const PaymentSuccess: React.FC = () => {
   const router = useRouter();
+  const dispatch = useDispatch(); // Redux dispatch
 
-  // Extraire orderId et payment_id de l'URL
+  // Extract orderId, paymentId, and restaurantId from URL
   const searchParams = new URLSearchParams(window.location.search);
   const orderId = searchParams.get("orderId");
   const paymentId = searchParams.get("payment_id");
+  const restaurantId = searchParams.get("restaurantId"); // Assuming restaurantId is passed
 
   useEffect(() => {
     if (!orderId || !paymentId) {
@@ -20,27 +25,50 @@ const PaymentSuccess: React.FC = () => {
         title: "Invalid parameters",
         text: "Order ID or Payment ID is missing, redirecting to home...",
         icon: "warning",
-        buttons: undefined, // Remplacer 'false' par 'undefined'
+        buttons: undefined, // Replace 'false' with 'undefined'
         timer: 3000,
       }).then(() => {
         router.push("/");
       });
+    } else {
+      // Update order success status
+      axios
+        .put(`${serverDomain}/api/orders/${orderId}/success`)
+        .then((response) => {
+          const restaurantId = response.data.order.restaurantId;
+          dispatch(removeItemsByRestaurantId(restaurantId));
+        })
+        .catch((error) => {
+          console.error("Error while updating the order", error);
+        });
+
+      // Update payment method
+      const updatePaymentMethod = async () => {
+        try {
+          const response = await axios.put(
+            `${serverDomain}/api/payment/update-payment-method`,
+            {
+              orderId,
+              paymentMethod: `flouci - pay infos : ${paymentId}`,
+            }
+          );
+
+          console.log("API Response:", response.data);
+        } catch (error) {
+          console.error("Error:", error);
+        }
+      };
+
+      // Call the function to update payment method
+      updatePaymentMethod();
     }
-  }, [orderId, paymentId, router]);
+  }, [orderId, paymentId, restaurantId, router, dispatch]);
 
   return (
     <div className="min-h-screen pb-20 mt-24 bg-gray-100">
       <header className="w-full bg-white">
         <div className="flex justify-between px-6 py-8 mx-auto max-w-7xl">
           <div>
-            {/* <button
-              onClick={() => router.push("/")}
-              className="inline-flex items-center px-4 py-1 mb-4 rounded-lg bg-primary text-dark hover:text-primary hover:bg-dark"
-            >
-              <AiOutlineArrowLeft className="w-5 h-5 mr-2" />
-              Back to Home
-            </button> */}
-
             <h1 className="text-5xl font-bold text-gray-900">
               Payment Successful
             </h1>
@@ -60,7 +88,7 @@ const PaymentSuccess: React.FC = () => {
             <div className="space-y-4">
               <div className="flex items-center justify-center">
                 <Image
-                  src={successImage.src} // Utilisation d'une image pour symboliser le succès
+                  src={successImage.src}
                   alt="Success"
                   width={200}
                   height={200}
